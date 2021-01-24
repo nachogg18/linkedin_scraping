436b94e7da1c2bf5dfa3ae2cf988d803c58c3d92
ignacio galvez <ignacio.galvez@alumnos.frm.utn.edu.ar>
2021-01-24 00:53:07 -0300

    added initial_setup for linkedin web scraping

--- linkedin.py
+++ linkedin.py
@@ -2,7 +2,21 @@ import pickle
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Firefox()
-driver.get("https://www.linkedin.com")
+def initial_setup():
+    #Creo el driver de firefox
+    driver = webdriver.Firefox()
+    #Dirijo a web de linkedin
+    driver.get("https://www.linkedin.com")
+    #deserializo las cookies,que se deben encontrar en el directorio raíz
+    cookies = pickle.load(open("cookies.pkl", "rb"))
+    #cargo las cookies de sesión
+    for cookie in cookies:
+        driver.add_cookie(cookie)
+    #actualizo la página
+    driver.get("http://www.linkedin.com")
+
+#lanzo el metodo de configuración inicial
+initial_setup()
+
 
 
